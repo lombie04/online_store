7cb687d70e90907ccfa29d664daa84228a3559f4
Salem Lombard <salemlombard3@gmail.com>
2026-02-04 14:03:12 +0100

    Fix root redirect for Render

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/includes/layout.php';
+
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/helpers.php';
 
@@ -8,5 +8,4 @@ if (is_logged_in()) {
     $u = current_user();
     redirect(role_home_path($u));
 }
-
-redirect('/business_store/home.php');
+redirect('/home.php');
