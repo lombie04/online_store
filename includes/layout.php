5d4a86029674289b36b55f970fcf646367833a89
Salem Lombard <salemlombard3@gmail.com>
2026-02-04 13:50:12 +0100

    Remove config.php dependency for Render

--- includes/layout.php
+++ includes/layout.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/helpers.php';
-require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../db.php';
 
 function layout_header(string $title): void
 {
